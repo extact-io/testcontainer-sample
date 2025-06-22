14a46671454b4d88d9010567aa4386002f881292
extact-io <ozytso@gmail.com>
2025-06-22 21:04:04 +0900

    upate

--- container-client/src/main/java/io/extact/sample/testcontainer/details/PropertiesUrlConnectionDetails.java
+++
@@ -1,9 +0,0 @@
-package io.extact.sample.testcontainer.details;
-
-import lombok.Data;
-
-@Data
-public class PropertiesUrlConnectionDetails implements UrlConnectionDetails {
-
-    private String url;
-}
--- container-client/src/main/java/io/extact/sample/testcontainer/details/UrlConnectionDetails.java
+++
@@ -1,8 +0,0 @@
-package io.extact.sample.testcontainer.details;
-
-import org.springframework.boot.autoconfigure.service.connection.ConnectionDetails;
-
-public interface UrlConnectionDetails extends ConnectionDetails {
-
-    String getUrl();
-}
